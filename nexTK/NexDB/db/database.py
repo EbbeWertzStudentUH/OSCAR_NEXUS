6e04b481a8308459dce489b17b557d05186cd49d
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2025-03-25 22:35:55 +0100

    made join generator (generates all sql joins given an implicit relation between two tables)

--- nexTK/NexDB/db/database.py
+++ nexTK/NexDB/db/database.py
@@ -2,7 +2,7 @@ import operator
 from networkx import Graph
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.sql.schema import Table
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Query, sessionmaker
 from models import Base, Batch, Dataset, Field, Schema
 
 DATABASE_URL = "sqlite:///NexDB.db"
@@ -27,8 +27,8 @@ def init_db():
 import networkx as nx
 from sqlalchemy.orm import class_mapper, aliased
 
-def build_dependency_graph(base) -> Graph:
-    graph = nx.Graph()  # Make it an undirected graph
+def build_dependency_graph(base: type) -> Graph:
+    graph = nx.Graph()
 
     for class_ in base.registry.mappers:
         clazz = class_.class_
@@ -49,6 +49,18 @@ def build_dependency_graph(base) -> Graph:
 
     return graph
 
+def generate_joins(joins: list, find_class: type, join_class: type, graph: Graph):
+
+    path = nx.shortest_path(graph, source=find_class.__table__, target=join_class.__table__)
+    
+    for i in range(len(path) - 1):
+        source_table = path[i]
+        target_table = path[i + 1]
+        source_field = graph[source_table][target_table]["source_field"]
+        target_field = graph[source_table][target_table]["target_field"]
+        joins.append((join_class, source_field, target_field))
+
+    return joins
 
 
 query = session.query(Batch)
@@ -56,7 +68,12 @@ graph = build_dependency_graph(Base)
 for node1, node2, data in graph.edges(data=True):
     print(f"Edge: {node1} <--> {node2}".ljust(40), end='')
     print(''.join([f"{at}: {val}".ljust(40) for at, val in data.items()]))
-
+    
+joins = []
+generate_joins(joins, Batch, Dataset, graph)
+generate_joins(joins, Batch, Field, graph)
+for j in joins:
+    print(''.join([f"{i}".ljust(30) for i in j]))
 print(str(query))
 # query_str = str(query.statement.compile(compile_kwargs={"literal_binds": True})) #literal binds, renders in the literals (otheriwse fields are compared against 'name_1' instead of the actual given name value)
 # print(query_str)
